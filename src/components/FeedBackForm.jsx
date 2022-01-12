55214884a34665d1b0dd5f396abd2cc4fc74120c
linkprada <haouaouchi.said@gmail.com>
2022-01-12 22:06:26 +0100

    Add Button Component

--- src/components/FeedBackForm.jsx
+++ src/components/FeedBackForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react/cjs/react.development"
+import Button from "./shared/Button";
 import Card from "./shared/Card"
 
 function FeedBackForm() {
@@ -14,7 +15,7 @@ function FeedBackForm() {
                 <h2>How do you rate this app</h2>
                 <div className="input-group">
                     <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-                    <button type="submit" className="btn btn-primary">Send</button>
+                    <Button type='submit' children='Send'></Button>
                 </div>
             </form>
         </Card>
