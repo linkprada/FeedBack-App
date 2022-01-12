b85b61c68570475b7b540bb70d407be65efd0ce2
linkprada <haouaouchi.said@gmail.com>
2022-01-12 23:27:57 +0100

    Add Validation Message

--- src/components/FeedBackForm.jsx
+++ src/components/FeedBackForm.jsx
@@ -4,8 +4,23 @@ import Card from "./shared/Card"
 
 function FeedBackForm() {
     const [text, setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [validationMessage, setValidationMessage] = useState("");
 
     var handleTextChange = (e) => {
+        if (text === "") {
+            setBtnDisabled(true);
+            setValidationMessage(null);
+        }
+        else if (text !== "" && text.trim().length < 10) {
+            setBtnDisabled(true);
+            setValidationMessage("Text must be at least 10 characters");
+        }
+        else {
+            setBtnDisabled(false);
+            setValidationMessage(null);
+        }
+        
         setText(e.target.value);
     }
 
@@ -15,9 +30,10 @@ function FeedBackForm() {
                 <h2>How do you rate this app</h2>
                 <div className="input-group">
                     <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-                    <Button type='submit' children='Send'></Button>
+                    <Button type='submit' isDisabled={btnDisabled} children='Send'></Button>
                 </div>
             </form>
+            {validationMessage && <div>{validationMessage}</div>}
         </Card>
     )
 }
